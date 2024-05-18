2e3068cf32699992f817be70b94085e2543a1e7a
T1WiLLi <williambeaudun@gmail.com>
2024-05-17 21:26:33 -0400

    Upgrade the Account Component, will work on the account security feature later

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom"
 import { User } from "../interface/user";
 import { Auth } from "../api/auth";
 import "../styles/pages/accounts.css";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, ListGroup, Button } from "react-bootstrap";
 import NotFound from "./notFound";
 
 
@@ -44,29 +44,105 @@ function Account() {
 
     return (
         <Container>
-            <Row className="justify-content-center mt-5">
-                <Col xs={12} md={6}>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Account Information</Card.Title>
-                            <Card.Text>
-                                <p>ID: {user.id}</p>
-                                <p>Name: {user.firstName} {user.lastName}</p>
-                                <p>Email: {user.email}</p>
-                                <p>Age: {user.age}</p>
-                                <p>Gender: {user.gender}</p>
-                                <p>Phone: {user.phone}</p>
-                                <p>Username: {user.username}</p>
-                                <p>Birth Date: {user.birthDate}</p>
-                                {/* Add more user information here */}
-                            </Card.Text>
-                        </Card.Body>
+            <Row className="justify-content-center mt-5 mb-5">
+                <Col xs={12} md={8}>
+                    <AccountInfo user={user} />
+                    <Card className="mb-3">
+                        <Card.Header>Additional Information</Card.Header>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <strong>Maiden Name:</strong> {user.maidenName}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Blood Group:</strong> {user.bloodGroup}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Height:</strong> {user.height} cm
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Weight:</strong> {user.weight} kg
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Eye Color:</strong> {user.eyeColor}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Hair Color:</strong> {user.hair.color}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Hair Type:</strong> {user.hair.type}
+                            </ListGroup.Item>
+                        </ListGroup>
                     </Card>
-                    <Link to="/">Go to Home</Link>
+                    <Card className="mb-3">
+                        <Card.Header>Financial Information</Card.Header>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <strong>Bank:</strong> {user.bank.cardNumber} ({user.bank.cardType})
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>IBAN:</strong> {user.bank.iban}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Currency:</strong> {user.bank.currency}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>EIN:</strong> {user.ein}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>SSN:</strong> {user.ssn}
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                    <div className="d-flex justify-content-between">
+                        <Link to="/" className="btn btn-primary">
+                            Go to Home
+                        </Link>
+                        <Button variant="secondary">Settings</Button>
+                    </div>
                 </Col>
             </Row>
         </Container>
     );
 }
+
+const AccountInfo: React.FC<{ user: User }> = ({ user }) => {
+    return (
+        <Card className="mb-3">
+            <Card.Header>Account Information</Card.Header>
+            <Card.Body>
+                <Row>
+                    <Col md={3}>
+                        <img src={user.image} alt="User Profile" className="img-fluid rounded" />
+                    </Col>
+                    <Col md={9}>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <strong>Name:</strong> {user.firstName} {user.lastName}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Email:</strong> {user.email}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Age:</strong> {user.age}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Gender:</strong> {user.gender}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Phone:</strong> {user.phone}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Username:</strong> {user.username}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Birth Date:</strong> {user.birthDate}
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    );
+};
 
 export default Account
